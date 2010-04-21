450bb59d29396a785baea8e9218fb332640a2622
Patrick Maia <patrickjourdanmaia@gmail.com>
2010-04-21 14:26:55 -0300

    Preparei o código pra implementar tipos de alocações utilizando decorators

---
+++ src/main/java/model/DiskAllocationDriver.java
@@ -0,0 +1,23 @@
+package model;
+
+/**
+ * 
+ * @author Patrick Maia
+ *
+ */
+public class DiskAllocationDriver implements AllocationDriver {
+
+	@Override
+	public byte[] read(int allocationStartOffset, int length) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void write(byte[] data, int dataStartOffset,
+			long allocationStartOffset, int length) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/model/MemoryAllocationDriver.java
@@ -0,0 +1,38 @@
+package model;
+
+/**
+ * 
+ * @author Patrick Maia
+ *
+ */
+public class MemoryAllocationDriver implements AllocationDriver {
+	
+	private byte [] data;
+	
+	public MemoryAllocationDriver(int size) {
+		data = new byte[size];
+	}
+
+	@Override
+	public byte[] read(int allocationStartOffset, int length) {
+		byte [] result = new byte[length];
+		
+		System.arraycopy(data, allocationStartOffset, result, 0, length);
+		
+		return result;
+	}
+
+	@Override
+	public void write(byte[] data, int dataStartOffset, long allocationStartOffset, int length) {
+		if(this.data.length < dataStartOffset + length) {
+			byte [] temp = new byte[dataStartOffset + length];
+			
+			System.arraycopy(this.data, 0, temp, 0, this.data.length);
+			
+			this.data = temp;
+		}
+		
+		System.arraycopy(data, dataStartOffset, this.data, (int)allocationStartOffset, length);
+	}
+
+}
